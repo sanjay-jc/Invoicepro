f22da96446d2ca08c7199d34205849a0f573f110
Sanjay Jc <67824341+sanjay-jc@users.noreply.github.com>
2024-03-28 11:28:10 +0530

    Update urls.py

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -3,12 +3,16 @@ from django.urls import path,include,re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenBlacklistView,
+TokenRefreshView
+    
 )
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="login"),
+    path("api/token/", TokenObtainPairView.as_view(), name="login"),
+     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path('account/',include("local_apps.accounts.urls")),
     path('invoice/',include("local_apps.invoices.urls")),
